d91fbebc9f1e33dee06eb8cd54f584b9b694c643
Lucas <ljkeller@ucdavis.edu>
2021-04-25 22:04:48 -0700

    Clearer sections

--- src/sections/components/SectionHeader.js
+++ src/sections/components/SectionHeader.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function SectionHeader({ children }) {
-  return <h3 className='text-4xl mb-8 font-mono px-2'>&gt; {children}</h3>;
+  return (
+    <h3 className='text-4xl mb-8 font-mono px-2 pb-4 border-b-2 border-black'>
+      &gt; {children}
+    </h3>
+  );
 }
